d1d05ca820ebb23b4cb8344136c1ee6f24769648
abdullahberkeozder <87164847+abdullahberkeozder@users.noreply.github.com>
2022-05-04 23:48:02 +0300

    C# Database Connection

---
+++ C# DB connection/Club.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Club
+{
+    private
+        int id;
+        string name;
+        int numberOfSupporters = 0;
+
+        public Club(int id, string name, int supporters)
+        {
+            this.Id = id;
+            this.name = name;
+            numberOfSupporters = supporters;
+        }
+        public Club() {
+        id = -1;
+        name = "Null";
+        numberOfSupporters = 0;
+        }
+        public void addSupporters()
+        {
+            numberOfSupporters++;
+        }
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int NumberOfSupporters { get => numberOfSupporters; set => numberOfSupporters = value; }
+}
